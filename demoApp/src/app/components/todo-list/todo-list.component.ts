c04e5d66e00bb2b1156cc1451bcf48565f00f81c
Suraj Negim <er.surajnegi@gmail.com>
2017-07-13 20:57:13 +0530

    h

--- demoApp/src/app/components/todo-list/todo-list.component.ts
+++ demoApp/src/app/components/todo-list/todo-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
-  private tasksData = mockData;
+  public tasksData = mockData;
   constructor() { }
 
   ngOnInit() { }
